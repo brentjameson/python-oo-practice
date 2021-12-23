01f292878a487d9d4020ebe9d52ec7374ebd5cba
Brent Jameson <brentjameson@Brents-MBP.attlocal.net>
2021-12-23 12:56:55 -0500

    completed exercise

--- serial.py
+++ serial.py
@@ -1,6 +1,9 @@
 """Python serial number generator."""
 
+import math
+
 class SerialGenerator:
+    counter = 0
     """Machine to create unique incrementing serial numbers.
     
     >>> serial = SerialGenerator(start=100)
@@ -20,3 +23,17 @@ class SerialGenerator:
     100
     """
 
+    def __init__(self, start):
+        self.start = start
+    
+    def reset(self):
+        print('doooough')
+        self.start = self.start - self.counter
+        return f"The serial number has been reset to it's original start number of {self.start}"
+
+    def generate(self):
+        self.start += 1
+        self.counter += 1
+        return self.start
+        # math.nextafter(self.start,math.inf)
+        
--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,34 @@
 """Word Finder: finds random words from a dictionary."""
+from random import randrange
 
 
 class WordFinder:
     ...
+    def __init__ (self, file_path):
+        self.file_path = file_path
+        with open(self.file_path, 'r') as words_list:
+            self.words_list = list(words_list)
+
+    def num_of_words_read(self):
+        return len(self.words_list)
+
+    def random_word(self):
+        random_index = randrange(0,self.num_of_words_read())
+        return self.words_list[random_index].rstrip('\n')
+
+class SpecialWordFinder(WordFinder):
+    def __init__(self, file_path):
+        super().__init__(file_path)
+
+    def random_word(self):
+        while(True):
+            random_word = super().random_word()
+            if random_word[0] != '#':
+                out = random_word
+                break
+        return out
+
+
+
+
+new_inst = SpecialWordFinder('words.txt')
